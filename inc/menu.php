b981b69caee75c929c4bf0197f4d89b857458b4a
joostdelange <post@joostdelange.nl>
2016-12-13 17:24:02 +0100

    Update

---
+++ inc/menu.php
@@ -0,0 +1,18 @@
+<header class="mdl-layout__header">
+                <div class="mdl-layout__header-row"> <span class="mdl-layout-title">Day Registration</span>
+                    <!-- Title -->
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+                    <!-- Navigation. We hide it in small screens. -->
+                    <nav class="mdl-navigation mdl-layout--large-screen-only">
+                        <?php include("link.php") ?>
+                    </nav>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer"> <span class="mdl-layout-title"><img src="img/logo.png">Day Registration</span>
+                <nav class="mdl-navigation mdl-js-ripple-effect">
+                    <?php include("link.php") ?>
+                </nav>
+            </div>
+            <main class="mdl-layout__content">
+                <div class="page-content">
--- index.php
+++ index.php
@@ -7,29 +7,11 @@
     <body>
         <!-- Always shows a header, even in smaller screens. -->
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-            <header class="mdl-layout__header">
-                <div class="mdl-layout__header-row"> <span class="mdl-layout-title">Dagen Registratie</span>
-                    <!-- Title -->
-                    <!-- Add spacer, to align navigation to the right -->
-                    <div class="mdl-layout-spacer"></div>
-                    <!-- Navigation. We hide it in small screens. -->
-                    <nav class="mdl-navigation mdl-layout--large-screen-only">
-                        <?php include("inc/link.php") ?>
-                    </nav>
-                </div>
-            </header>
-            <div class="mdl-layout__drawer"> <span class="mdl-layout-title"><img src="img/logo.png">Dagen Registratie</span>
-                <nav class="mdl-navigation">
-                    <?php include("inc/link.php") ?>
-                </nav>
-            </div>
-            <main class="mdl-layout__content">
-                <div class="page-content">
+            <?php
+                include("inc/menu.php");
+            ?>
                     <!-- Your content goes here -->
                     <!-- Wide card with share menu button -->
-                    <style>
-
-                    </style>
                     <div class="card-wide mdl-card mdl-shadow--2dp">
                         <div class="mdl-card__title">
                             <h2 class="mdl-card__title-text">Welcome</h2> </div>
@@ -45,5 +27,4 @@
             </main>
         </div>
     </body>
-
 </html>
