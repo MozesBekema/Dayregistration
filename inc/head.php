1ec3c77a9b4a83834fe8d968a1f860f2587a5e78
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-13 09:31:45 +0100

    Update

--- inc/head.php
+++ inc/head.php
@@ -1,5 +1,8 @@
 <head>
-    <link href="../css/styles.css" rel="stylesheet" type="text/css">
-    <link href="../css/material.css" rel="stylesheet" type="text/css">
-    <script src="../js/material.min.js" type="text/javascript"></script>
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/material.css" rel="stylesheet" type="text/css">
+    <script src="js/material.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
