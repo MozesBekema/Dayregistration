51c5e9599c4cdb45c7ea37b32fda6061a64d23e8
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-13 09:20:46 +0100

    Update

--- inc/head.php
+++ inc/head.php
@@ -1,3 +1,5 @@
 <head>
-    <link href="styles.css" rel="stylesheet" type="text/css">
+    <link href="../css/styles.css" rel="stylesheet" type="text/css">
+    <link href="../css/material.css" rel="stylesheet" type="text/css">
+    <script src="../js/material.min.js" type="text/javascript"></script>
 </head>
