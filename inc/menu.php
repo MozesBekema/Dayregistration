e51f5cff4f5dabe96261b44be534ce99936a4fd7
joostdelange <post@joostdelange.nl>
2016-12-14 21:02:09 +0100

    Update

--- inc/menu.php
+++ inc/menu.php
@@ -5,13 +5,13 @@
                     <div class="mdl-layout-spacer"></div>
                     <!-- Navigation. We hide it in small screens. -->
                     <nav class="mdl-navigation mdl-layout--large-screen-only">
-                        <?php include("link.php") ?>
+                        <a class="mdl-navigation__link" href="" >Login</a>
                     </nav>
                 </div>
             </header>
             <div class="mdl-layout__drawer"> <span class="mdl-layout-title"><a href="index.php"><img src="img/logo.png"></a>Day Registration</span>
                 <nav class="mdl-navigation mdl-js-ripple-effect">
-                    <?php include("link.php")?>
+                    <a class="mdl-navigation__link" href="" >Login</a>
                 </nav>
             </div>
 
