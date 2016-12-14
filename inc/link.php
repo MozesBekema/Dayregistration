fcb685304869ec279b7fe730468a27f38e35d999
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-14 11:41:30 +0100

    Update

--- inc/link.php
+++ inc/link.php
@@ -1,1 +1,24 @@
-<a class="mdl-navigation__link" href="">Login</a>
+<a class="mdl-navigation__link dialog-button" id="show-dialog">Login</a>
+<dialog class="mdl-dialog">
+    <button type="button" class="mdl-button close"><i class="material-icons">clear</i></button>
+    <h4 class="mdl-dialog__title"><img src="img/logo.png">Create account</h4>
+    <div class="mdl-dialog__content">
+        <form action="#">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample3">
+                <label class="mdl-textfield__label" for="sample3">Username</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample3">
+                <label class="mdl-textfield__label" for="sample3">Email</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample3">
+                <label class="mdl-textfield__label" for="sample3">Password</label>
+            </div>
+            <div class="mdl-dialog__actions">
+                <button type="button" class="mdl-button">Create</button>
+            </div>
+        </form>
+    </div>
+</dialog>
