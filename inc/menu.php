fa8600662e0ecd5947c0926c6d3cec7bca50579b
joostdelange <post@joostdelange.nl>
2016-12-14 22:15:34 +0100

    Update

--- inc/menu.php
+++ inc/menu.php
@@ -1,17 +1,16 @@
 <header class="mdl-layout__header">
-                <div class="mdl-layout__header-row"> <span class="mdl-layout-title">Day Registration</span>
-                    <!-- Title -->
-                    <!-- Add spacer, to align navigation to the right -->
-                    <div class="mdl-layout-spacer"></div>
-                    <!-- Navigation. We hide it in small screens. -->
-                    <nav class="mdl-navigation mdl-layout--large-screen-only">
-                        <a class="mdl-navigation__link" href="" >Login</a>
-                    </nav>
-                </div>
-            </header>
-            <div class="mdl-layout__drawer"> <span class="mdl-layout-title"><a href="index.php"><img src="img/logo.png"></a>Day Registration</span>
-                <nav class="mdl-navigation mdl-js-ripple-effect">
-                    <a class="mdl-navigation__link" href="" >Login</a>
-                </nav>
-            </div>
-
+    <div class="mdl-layout__header-row"> <span class="mdl-layout-title">Day Registration</span>
+        <!-- Title -->
+        <!-- Add spacer, to align navigation to the right -->
+        <div class="mdl-layout-spacer"></div>
+        <!-- Navigation. We hide it in small screens. -->
+        <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonRed">Login</button>
+        </nav>
+    </div>
+</header>
+<div class="mdl-layout__drawer"> <span class="mdl-layout-title"><a href="index.php"><img src="img/logo.png"></a>Day Registration</span>
+    <nav class="mdl-navigation mdl-js-ripple-effect">
+        <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonBlack">Login</button>
+    </nav>
+</div>
--- inc/register.php
+++ inc/register.php
@@ -1,20 +1,20 @@
 <div class="mdl-card__actions mdl-card--border">
-    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect dialog-button" id="show-dialog">Get Started</a>
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect dialog-button" id="dialogReg">Get Started</a>
         <dialog class="mdl-dialog">
             <button type="button" class="mdl-button close"><i class="material-icons">clear</i></button>
             <h4 class="mdl-dialog__title"><img src="img/logo.png">Create account</h4>
             <div class="mdl-dialog__content">
                 <form action="#">
                     <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                        <input class="mdl-textfield__input" type="text" id="sample3">
+                        <input class="mdl-textfield__input" type="text">
                         <label class="mdl-textfield__label" for="sample3">Username</label>
                     </div>
                     <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                        <input class="mdl-textfield__input" type="text" id="sample3">
+                        <input class="mdl-textfield__input" type="text">
                         <label class="mdl-textfield__label" for="sample3">Email</label>
                     </div>
                     <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                        <input class="mdl-textfield__input" type="text" id="sample3">
+                        <input class="mdl-textfield__input" type="text">
                         <label class="mdl-textfield__label" for="sample3">Password</label>
                     </div>
                     <div class="mdl-dialog__actions">
@@ -23,7 +23,19 @@
                 </form>
             </div>
         </dialog>
-        <?php include("dialog.php");?>
+        <script>
+            var dialog = document.querySelector('dialog');
+            var showDialogButton = document.querySelector('#dialogReg');
+            if (! dialog.showModal) {
+                dialogPolyfill.registerDialog(dialog);
+                }
+            showDialogButton.addEventListener('click', function() {
+                dialog.showModal();
+            });
+            dialog.querySelector('.close').addEventListener('click', function() {
+                dialog.close();
+            });
+        </script>
         <a href="index.php">
             <div class="mdl-card__menu">
                 <div id="tt4" class="icon material-icons">share</div>
