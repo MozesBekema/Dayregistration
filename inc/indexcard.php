69232c0ee26ea481094b783d62d24497adda20e1
joostdelange <post@joostdelange.nl>
2016-12-15 11:26:26 +0100

    Update

---
+++ inc/indexcard.php
@@ -0,0 +1,14 @@
+<div class="card-wide mdl-card mdl-shadow--2dp">
+    <div class="mdl-card__title">
+        <p>Day Registration, a simple material web application to track the days you have worked.</p>
+    </div>
+<div class="mdl-card__supporting-text">
+    <br/>
+    With the technology and software of today there are a lot of options to keep track of your work activities. Now the problem with all this software is that it is quite complicated to keep it up and don't forget to fill in what you did and for how many hours.<br/>
+    <br/>
+    That's why we made this web application, for you. Anything you find on this platform will be as simple and understandable as posible so you can start working with it immediately.<br/>
+    <br>
+    Now, go ahead and click below.
+</div>
+<?php include("register.php");?>
+</div>
