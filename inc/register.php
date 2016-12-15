5deab678d7eaac8727c276f37ec77ac2209af0a0
joostdelange <post@joostdelange.nl>
2016-12-15 11:11:24 +0100

    Update

--- inc/register.php
+++ inc/register.php
@@ -1,7 +1,7 @@
 <div class="mdl-card__actions mdl-card--border">
     <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect dialog-button" id="dialogReg">Get Started</a>
         <dialog class="mdl-dialog">
-            <button type="button" class="mdl-button close"><i class="material-icons">clear</i></button>
+            <button type="button" class="mdl-button mdl-js-button mdl-js-ripple-effect close"><i class="material-icons">clear</i></button>
             <h4 class="mdl-dialog__title"><img src="img/logo.png">Create account</h4>
             <div class="mdl-dialog__content">
                 <form action="#">
@@ -18,7 +18,7 @@
                         <label class="mdl-textfield__label" for="sample3">Password</label>
                     </div>
                     <div class="mdl-dialog__actions">
-                        <button type="button" class="mdl-button">Create</button>
+                        <button type="button" class="mdl-button mdl-js-button mdl-js-ripple-effect">Create</button>
                     </div>
                 </form>
             </div>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <?php
         include("inc/head.php");
+        include("inc/random.php");
     ?>
     <body>
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -10,9 +11,10 @@
             ?>
             <div class="card-wide mdl-card mdl-shadow--2dp">
                 <div class="mdl-card__title">
-                    <h2 class="mdl-card__title-text">Welcome</h2>
+                    <p>Day Registration, a simple material web application to track the days you have worked.</p>
                 </div>
                 <div class="mdl-card__supporting-text">
+                    <br/>
                     With the technology and software of today there are a lot of options to keep track of your work activities. Now the problem with all this software is that it is quite complicated to keep it up and don't forget to fill in what you did and for how many hours.<br/>
                     <br/>
                     That's why we made this web application, for you. Anything you find on this platform will be as simple and understandable as posible so you can start working with it immediately.<br/>
