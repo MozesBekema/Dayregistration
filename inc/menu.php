8796048c2ad3ea74fb0864ae2ddac1fcff1e47be
joostdelange <post@joostdelange.nl>
2016-12-20 11:51:39 +0100

    Update

--- inc/menu.php
+++ inc/menu.php
@@ -1,17 +1,26 @@
 <header class="mdl-layout__header">
     <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title mdl-js-button mdl-js-ripple-effect">Day Registration</span>
+        <a href="index.php"><span class="mdl-layout-title mdl-js-button mdl-js-ripple-effect">Day Registration</span></a>
         <!-- Title -->
         <!-- Add spacer, to align navigation to the right -->
         <div class="mdl-layout-spacer"></div>
         <!-- Navigation. We hide it in small screens. -->
         <nav class="mdl-navigation mdl-layout--large-screen-only">
-            <a href="login.php"><button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonRed">Login</button></a>
+            <a href="login.php">
+                <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonRed">Login</button>
+            </a>
         </nav>
     </div>
 </header>
-<div class="mdl-layout__drawer"> <span class="mdl-layout-title"><a href="index.php"><img src="img/logo.png"></a>Day Registration</span>
+<div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">
+        <a href="index.php">
+            <img src="img/logo.png">
+        </a>Day Registration
+    </span>
     <nav class="mdl-navigation mdl-js-ripple-effect">
-        <a href="login.php"><button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonBlack">Login</button></a>
+        <a href="login.php">
+            <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonBlack">Login</button>
+        </a>
     </nav>
 </div>
--- login.php
+++ login.php
@@ -1,25 +1,37 @@
+<!doctype html>
 <html>
     <?php
         include("inc/head.php");
     ?>
     <body>
-        <?php
-            include("inc/menu.php");
-        ?>
-        <div class="card-wide mdl-card card-wide2 mdl-card2 mdl-shadow--2dp">
-            <form method="post" class="loginForm">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" type="text" name="username" required>
-                    <label class="mdl-textfield__label" for="sample3">Username</label>
-                </div><br/>
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input passwordMargin" type="password" name="password" required>
-                    <label class="mdl-textfield__label" for="sample3">Password</label>
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <a href="index.php"><span class="mdl-layout-title mdl-js-button mdl-js-ripple-effect">Day Registration</span></a>
+                    <!-- Title -->
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+                    <!-- Navigation. We hide it in small screens. -->
+                    <nav class="mdl-navigation mdl-layout--large-screen-only">
+                        <a href="index.php">
+                            <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonRed">Back</button>
+                        </a>
+                    </nav>
                 </div>
-                <div class="mdl-dialog__actions">
-                    <button type="submit" name="submit" class="mdl-button mdl-js-button mdl-js-ripple-effect">Login</button>
-                </div>
-            </form>
+            </header>
+            <div class="mdl-layout__drawer">
+                <span class="mdl-layout-title">
+                    <a href="index.php">
+                        <img src="img/logo.png">
+                    </a>
+                    Day Registration
+                </span>
+                <nav class="mdl-navigation mdl-js-ripple-effect">
+                    <a href="index.php">
+                        <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonBlack">Back</button>
+                    </a>
+                </nav>
+            </div>
         </div>
     </body>
 </html>
