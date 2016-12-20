64375d3fe5da78d36f9bd4dd12ca5a6b8b077db4
joostdelange <post@joostdelange.nl>
2016-12-20 22:13:55 +0100

    Update

--- inc/register.php
+++ inc/register.php
@@ -3,8 +3,8 @@
         <dialog class="mdl-dialog">
             <button type="button" class="mdl-button mdl-js-button mdl-js-ripple-effect close"><i class="material-icons">clear</i></button>
             <h4 class="mdl-dialog__title"><img src="img/logo.png">Create account</h4>
-            <div class="mdl-dialog__content">
                 <form action="" method="post">
+                    <br/><br/>
                     <div class="mdl-textfield textfieldPadding mdl-js-textfield mdl-textfield--floating-label">
                         <input class="mdl-textfield__input" type="text" name="username" required>
                         <label class="mdl-textfield__label" for="sample3">Username</label>
@@ -18,10 +18,9 @@
                         <label class="mdl-textfield__label" for="sample3">Password</label>
                     </div>
                     <div class="mdl-dialog__actions">
-                        <button type="submit" name="submit" class="mdl-button mdl-js-button mdl-js-ripple-effect">Create</button>
+                        <button type="submit" name="submitReg" class="mdl-button mdl-js-button mdl-js-ripple-effect">Create</button>
                     </div>
                 </form>
-            </div>
         </dialog>
         <script>
             var dialog = document.querySelector('dialog');
--- login.php
+++ login.php
@@ -32,6 +32,22 @@
                     </a>
                 </nav>
             </div>
+            <div class="mdl-dialog__content"><br/>
+                <h4 class="mdl-dialog__title"><img src="img/logo.png">Create account</h4><br/><br/>
+                <form action="" method="post">
+                    <div class="mdl-textfield textfieldPadding mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="text" name="username" required>
+                        <label class="mdl-textfield__label">Username</label>
+                    </div>
+                    <div class="mdl-textfield textfieldPadding mdl-js-textfield mdl-textfield--floating-label">
+                        <input class="mdl-textfield__input" type="password" name="password" required>
+                        <label class="mdl-textfield__label">Password</label>
+                    </div>
+                    <div class="mdl-dialog__actions">
+                        <button type="submit" name="submit" class="mdl-button mdl-js-button mdl-js-ripple-effect">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </body>
 </html>
