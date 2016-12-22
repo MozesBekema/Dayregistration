d5e336d9f675afb0bdc84b0aa55f44de39370460
joostdelange <post@joostdelange.nl>
2016-12-22 10:12:07 +0100

    Update

--- login.php
+++ login.php
@@ -21,8 +21,6 @@ session_start();
             echo"Wrong username or password";
         }
     }
-
-
 ?>
 <html>
     <?php
@@ -30,33 +28,9 @@ session_start();
     ?>
     <body>
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-            <header class="mdl-layout__header">
-                <div class="mdl-layout__header-row">
-                    <a href="index.php"><span class="mdl-layout-title mdl-js-button mdl-js-ripple-effect">Day Registration</span></a>
-                    <!-- Title -->
-                    <!-- Add spacer, to align navigation to the right -->
-                    <div class="mdl-layout-spacer"></div>
-                    <!-- Navigation. We hide it in small screens. -->
-                    <nav class="mdl-navigation mdl-layout--large-screen-only">
-                        <a href="index.php">
-                            <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonRed">Back</button>
-                        </a>
-                    </nav>
-                </div>
-            </header>
-            <div class="mdl-layout__drawer">
-                <span class="mdl-layout-title">
-                    <a href="index.php">
-                        <img src="img/logo.png">
-                    </a>
-                    Day Registration
-                </span>
-                <nav class="mdl-navigation mdl-js-ripple-effect">
-                    <a href="index.php">
-                        <button class="mdl-button mdl-js-button mdl-js-ripple-effect dialog-button buttonBlack">Back</button>
-                    </a>
-                </nav>
-            </div>
+            <?php
+                include("inc/loginmenu.php");
+            ?>
             <div class="mdl-dialog__content"><br/>
                 <h4 class="mdl-dialog__title"><img src="img/logo.png">Create account</h4><br/><br/>
                 <?php
@@ -78,7 +52,6 @@ session_start();
                 <?php
                     }
                     else{
-                        echo "<a href='login.php?action=logout'>Log uit</a>";
                         header('Location: main.php');
                     }
              ?>
--- main.php
+++ main.php
@@ -1,9 +1,28 @@
 <?php
-session_start();
+    session_start();
+    if(!isset($_SESSION['login_user'])){
+        header('Location:index.php');
+    }
 ?>
 <html>
     <?php
         include("inc/head.php");
-        include("inc/menu.php");
     ?>
+    <body>
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <?php
+                include("inc/menu.php");
+            ?>
+            <br/>
+                <h4 class="mdl-dialog__title"><img src="img/logo.png" class="logoImg">Statistieken</h4><br/><br/>
+                <?php
+                    if(!isset($_SESSION['login_user'])){
+                ?>
+                <?php
+                    }
+             ?>
+        </div>
+    </body>
+</html>
+
 
