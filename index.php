59db4afd7151ac2115ac5bc65c4d1559b0cfaaf3
joostdelange <post@joostdelange.nl>
2017-01-10 09:29:37 +0100

    Update HUGE

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Dayregistration</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,500" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        <div class="menuBar">
+            <div class="logo"></div>
+            <p class="dayRegistration">Day Registration</p>
+            <button class="buttonRaised">LOGIN</button>
+        </div>
+        <div class="headerBackground">
+            <div class="materialCalendar shadow shadowBox">
+                <div class="topRowCalendar">
+                    <div class="wrapper">
+                        <div class="holes"></div>
+                    </div>
+                    <div class="wrapper">
+                        <div class="holes"></div>
+                    </div>
+                    <div class="wrapper">
+                        <div class="holes"></div>
+                    </div>
+                </div>
+                <div class="selectionsCalendar">
+                    <i class="material-icons">check</i>
+                    <div class="lineLong"></div>
+                    <div class="lineShort"></div>
+                </div>
+                <div class="selectionsCalendar">
+                    <i class="material-icons">check</i>
+                    <div class="lineLong"></div>
+                    <div class="lineShort"></div>
+                </div>
+                <div class="selectionsCalendar">
+                    <i class="material-icons">check</i>
+                    <div class="lineLong"></div>
+                    <div class="lineShort"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="headerBackgroundLighter">
+            <div class="wrapperHeader">
+                <div class="container">
+                    <div class="fabButton">
+                        <i class="material-icons">access_time</i>
+                    </div>
+                    <h1>Safe time</h1>
+                    <p>Don’t worry about the hours you’ve worked, set the time registration on automatic and make adjustments later on.</p>
+                </div>
+            </div>
+            <div class="wrapperHeader">
+                <div class="container">
+                    <div class="fabButton">
+                        <i class="material-icons">phone_iphone</i>
+                    </div>
+                    <h1>On the go?</h1>
+                    <p>Day Registration is a fully functional web application that is user friendly on all devices.</p>
+                </div>
+            </div>
+            <div class="wrapperHeader">
+                <div class="container">
+                    <div class="fabButton">
+                        <i class="material-icons">brush</i>
+                    </div>
+                    <h1>Customize</h1>
+                    <p>We think the material design is already very beautiful but you can customize a lot to your own taste if you like.</p>
+                </div>
+            </div>
+        </div>
+        <div class="headerBackgroundLighterNoshadow">
+            <div class="containerBottom">
+                <div class="getStartedDraw">
+                    <div class="orangeFirstHalf">
+                        <div class="clock">
+                            <div class="pointerTall"></div>
+                            <div class="pointerSmall"></div>
+                        </div>
+                    </div>
+                    <div class="orangeSecondHalf">
+                        <div class="timer"></div>
+                        <div class="timerTop"></div>
+                        <div class="timerLeft"></div>
+                        <div class="timerRight"></div>
+                        <div class="digitalClock">
+                            <div class="digitalClockLighter">
+                                <p>10:08</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="getStarted">
+                    <button>GET STARTED</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
