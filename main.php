6b18b3280cc88d89426ff9f3d91006535e3b4572
Thomas Bekema <Thomas Bekema>
2017-01-17 10:38:38 +0100

    Update

--- main.php
+++ main.php
@@ -6,6 +6,28 @@
             session_destroy();
             header("Location: index.php");
     }
+
+
+
+$stageDays = 365;
+$username = $_SESSION['login_user'];
+
+if(isset($_POST['addButton'])){
+
+      $daysAdd = $conn->prepare("UPDATE `users` SET `days_worked`= `days_worked` + 1 WHERE`username` =  :username");
+      $daysAdd->execute(array('username'=>$username));
+}
+
+if(isset($_POST['minusButton'])){
+
+      $daysRemove = $conn->prepare("UPDATE `users` SET `days_worked`= `days_worked` - 1 WHERE`username` =  :username");
+      $daysRemove->execute(array('username'=>$username));
+}
+
+
+$days = $conn->prepare("SELECT `days_worked` FROM `users` WHERE `username` = :username");
+$days->execute(array('username'=>$username));
+
 ?>
     <!doctype>
     <html>
@@ -39,9 +61,39 @@
                 </a>
             </ul>
         </div>
-        <div class="addDay">
-            <button class="addButton">+</button>
+        <div class="add">
+            <form method="post" action="">
+                <button class="addButton" name="addButton" type="submit">+</button>
+                <button class="minusButton" name="minusButton" type="submit" >-</button>
+            </form>
         </div>
+        <?php
+         foreach($days as $row){
+        ?>
+        <div id="progress">
+            <div class="progressResult">
+                 <?php
+                    $percentage = $row['days_worked'] / $stageDays * 100;
+                    echo $percentage . "%" ;
+                ?>
+            </div>
+            <div class="progressCurrent">
+                 <?php
+                    echo $row['days_worked'];
+                ?>
+            </div>
+            <div class="progressDays">
+                 <?php
+                    $daysLeft = $stageDays  -  $row['days_worked'] ;
+                    echo $daysLeft;
+                ?>
+            </div>
+        </div>
+        <?php
+         }
+        ?>
+
+
     </body>
 
     </html>
