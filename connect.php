4cbb576f107fa7eb36a728067501d0e158b440b3
joostdelange <post@joostdelange.nl>
2017-01-23 12:00:59 +0100

    Update

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $conn = new PDO('mysql:host=db.joostdelange.nl;dbname=daenrjy158_dr',"daenrjy158_dr", "g7oVycQfH2");
+        $conn = new PDO('mysql:host=10.3.1.108;dbname=daenrjy158_dr',"daenrjy158_dr", "g7oVycQfH2");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $e) {
